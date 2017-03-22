2dde2b25921b0d4f3a49cff6406b76622ddb25de
rishona <rishona>
2017-03-22 16:57:48 -0400

    +simple table

--- popup.js
+++ popup.js
@@ -3,6 +3,20 @@ window.onload=main;
 function main()
 {
     chrome.storage.local.get("pointsdata",function(d){
+        var html="";
+        d.pointsdata.forEach(function(e){
+            html+=genTableEntry(e);
+        });
+
+        var ipoint=document.querySelector(".tdata tbody");
+
+        ipoint.innerHTML=ipoint.innerHTML+html;
+    });
+}
+
+function main2()
+{
+    chrome.storage.local.get("pointsdata",function(d){
         console.log(d.pointsdata);
 
         var idata=document.querySelector(".data");
@@ -13,4 +27,15 @@ function main()
 
         idata.innerHTML=datah;
     });
+}
+
+function genTableEntry(data)
+{
+    return `<tr>
+<td>${data.time}</td>
+<td>${data.points[0]}</td>
+<td>${data.points[1]}</td>
+<td>${data.points[2]}</td>
+<td>${data.points[3]}</td>
+</tr>\n`;
 }
